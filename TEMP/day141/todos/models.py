2491fe4404d2e67b7f7628d98b160e3e77351996
ungseo <ungseo@gmail.com>
2023-04-11 17:55:22 +0900

    1

--- TEMP/day141/todos/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from accounts.models import User
-
-# Create your models here.
-class Todo(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    title = models.CharField(max_length=100)
-    completed = models.BooleanField(default=False)
-    
